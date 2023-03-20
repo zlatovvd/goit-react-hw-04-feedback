0e8ccfe5b91441709bf6a614807972445f7e73d0
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-03-20 23:54:44 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,21 @@ import Statistics from './Statistics';
 import Notification from './Notification';
 
 const App = () => {
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const onLeaveFeedback = feedback => {
+
     switch (feedback) {
       case 'Good':
-        setGood(prevState => ({ good: prevState.good + 1 }));
+        setGood(prevState => prevState + 1);
         break;
       case 'Neutral':
-        setNeutral(prevState => ({ neutral: prevState.neutral + 1 }));
+        setNeutral(prevState => prevState + 1);
         break;
       case 'Bad':
-        setBad(prevState => ({ bad: prevState.bad + 1 }));
+        setBad(prevState => prevState + 1);
         break;
       default:
     }
@@ -27,11 +27,11 @@ const App = () => {
 
   const countTotalFeedback = () => {
     return good + neutral + bad;
-  }
+  };
 
   const countPositiveFeedbackPercentage = () => {
     return Math.round((good / countTotalFeedback()) * 100);
-  }
+  };
 
   return (
     <div
@@ -64,7 +64,76 @@ const App = () => {
       </Section>
     </div>
   );
+};
 
-}
+// class AppOld extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   onLeaveFeedback = feedback => {
+//     switch (feedback) {
+//       case 'Good':
+//         this.setState(prevState => ({ good: prevState.good + 1 }));
+//         break;
+//       case 'Neutral':
+//         this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+//         break;
+//       case 'Bad':
+//         this.setState(prevState => ({ bad: prevState.bad + 1 }));
+//         break;
+//       default:
+
+//     }
+//   };
+
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   }
+
+//   countPositiveFeedbackPercentage() {
+//     const { good } = this.state;
+//     return Math.round((good / this.countTotalFeedback()) * 100);
+//   }
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+
+//     return (
+//       <div
+//         style={{
+//           height: '100vh',
+//           paddingLeft: '40px',
+//           fontSize: 40,
+//           color: '#010101',
+//         }}
+//       >
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={['Good', 'Neutral', 'Bad']}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() > 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage()}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
